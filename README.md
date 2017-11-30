1c163915e21fea8e1d01eaa02a08a54c1fc1be19
Double <fangmingdong0224@gmail.com>
2017-11-30 23:29:30 +0800

    add

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+所有 Android Demo
+
+
+# TouchHandler
+
+触摸事件处理 Demo
+
--- touchhandler/src/main/java/com/fangmingdong/touchhandler/TouchHandlerMainActivity.java
+++ touchhandler/src/main/java/com/fangmingdong/touchhandler/TouchHandlerMainActivity.java
@@ -1,17 +1,13 @@
-package com.fangmingdong.androiddemo;
+package com.fangmingdong.touchhandler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TouchHandlerMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
+        setContentView(R.layout.touch_handler_main_activity);
     }
 }
