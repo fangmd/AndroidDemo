dd6964e1b80d95d2c00531e22bdefb1972d09ccb
方明东 <fangmingdong0224@gmail.com>
2017-12-01 14:49:26 +0800

    add

--- touchhandler/src/main/java/com/fangmingdong/touchhandler/print/PrintEventActivity.java
+++ touchhandler/src/main/java/com/fangmingdong/touchhandler/print/PrintEventActivity.java
@@ -1,5 +1,6 @@
 package com.fangmingdong.touchhandler.print;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +10,14 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
 import android.widget.TextView;
 
 import com.fangmingdong.touchhandler.R;
 
 public class PrintEventActivity extends AppCompatActivity {
 
+    private static final String TAG = PrintEventActivity.class.getSimpleName();
     private TextView mTv;
     private View mTouchView;
     private GestureDetectorCompat mDetector;
@@ -37,16 +40,105 @@ public class PrintEventActivity extends AppCompatActivity {
         mDetector = new GestureDetectorCompat(this, new MyGestureListener());
 
         mTouchView.setOnTouchListener(new View.OnTouchListener() {
+
+            private float mStartY;
+            private float mStartX;
+
+            private float mDownViewX;
+            private float mDownViewY;
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                mDetector.onTouchEvent(event);
+
+                if (mIsAnimating) {
+                    return false;
+                }
+
+//                mDetector.onTouchEvent(event);
+
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        mDownViewX = mTouchView.getX();
+                        mDownViewY = mTouchView.getY();
+
+                        mStartX = event.getRawX();
+                        mStartY = event.getRawY();
+                        Log.d(TAG, "onTouch: ACTION_DOWN   getRawX=" + mStartX + ",getRawY=" + mStartY);
+                        Log.d(TAG, "onTouch: ACTION_DOWN   x=" + mDownViewX + ",y=" + mDownViewY);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+
+                        float newX = event.getRawX();
+                        float newY = event.getRawY();
+                        float offsetX = newX - mStartX;
+                        float offsetY = newY - mStartY;
+
+                        mTouchView.setX(mTouchView.getX() + offsetX);
+                        mTouchView.setY(mTouchView.getY() + offsetY);
+
+                        Log.d(TAG, "onTouch: ACTION_MOVE   offsetX=" + offsetX +
+                                ",offsetY=" + offsetY);
+
+                        mStartX = newX;
+                        mStartY = newY;
+
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+                        //
+                        autoScrollTo(mDownViewX, mDownViewY);
+
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+
+                        break;
+                }
                 return true;
             }
         });
+    }
 
+    private boolean mIsAnimating;
+    private void autoScrollTo(float x, float y) {
+        Log.d(TAG, "autoScrollTo: x=" + x + ", y=" + y + ",, mTouchView.getX()" + mTouchView.getX() +
+                ",,mTouchView.getY()" + mTouchView.getY());
+        ViewPropertyAnimator animate = mTouchView.animate();
+        animate.translationXBy(x - mTouchView.getX());
+        animate.translationYBy(y - mTouchView.getY());
+        animate.setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mIsAnimating = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mIsAnimating = false;
+                Log.d(TAG, "onAnimationEnd: x=" + mTouchView.getX() + ", y=" + mTouchView.getY());
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        animate.setDuration(800);
+        animate.start();
+
+//        mTouchView.setX(x);
+//        mTouchView.setY(y);
     }
 
 
+
+
+    //
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
 
         private static final String DEBUG_TAG = "Gestures";
@@ -65,15 +157,8 @@ public class PrintEventActivity extends AppCompatActivity {
             int x = ((int) (startX - ((int) distanceX)));
             int y = ((int) (startY - ((int) distanceY)));
 
-
-
-
-
-//            mTouchView.setX(x);
-//            mTouchView.setY(y);
-
-//            mTouchView.setTranslationX(x);
-//            mTouchView.setTranslationY(y);
+            mTouchView.setX(x);
+            mTouchView.setY(y);
 
 
             mTv.setText("distanceX=" + distanceX + ", distanceY=" + distanceY + ", x=" + x + "&&y=" + y);
