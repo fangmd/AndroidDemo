2a413bc13643a855b6ced0c0f631aa955c1aeafd
Double <fangmingdong0224@gmail.com>
2017-12-11 18:59:19 +0800

    add

--- app/src/main/java/com/fangmingdong/androiddemo/coordinatorlayout/WangYiYun/YouDaoMineActivity.java
+++ app/src/main/java/com/fangmingdong/androiddemo/coordinatorlayout/WangYiYun/YouDaoMineActivity.java
@@ -3,13 +3,51 @@ package com.fangmingdong.androiddemo.coordinatorlayout.WangYiYun;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fangmingdong.androiddemo.R;
+import com.fangmingdong.androiddemo.widgets.SimpleTextAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class YouDaoMineActivity extends AppCompatActivity {
 
-    public static void actionStart(Context context){
+    private static final String TAG = YouDaoMineActivity.class.getSimpleName();
+    @BindView(R.id.toolbar_youdao)
+    Toolbar mToolbar;
+    @BindView(R.id.appbar_youdao)
+    AppBarLayout mAppbar;
+    @BindView(R.id.coordinator_youdao)
+    CoordinatorLayout mCoordinator;
+    @BindView(R.id.rv_youdao)
+    RecyclerView mRv;
+    @BindView(R.id.fab_youdao)
+    FloatingActionButton mFab;
+    @BindView(R.id.toolbar_title)
+    TextView mToolbarTitle;
+    @BindView(R.id.toolbar_edit)
+    TextView mToolbarEdit;
+    @BindView(R.id.collapsing_youdao)
+    CollapsingToolbarLayout mCollapsing;
+    @BindView(R.id.iv_toolbar)
+    ImageView mIvToolbar;
+
+    public static void actionStart(Context context) {
         Intent intent = new Intent(context, YouDaoMineActivity.class);
         context.startActivity(intent);
     }
@@ -18,5 +56,74 @@ public class YouDaoMineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wang_yi_yun_mine);
+        ButterKnife.bind(this);
+
+        mAppbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+
+            private float mLastOffsetPercent;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                float density = getResources().getDisplayMetrics().density;
+                Log.d(TAG, "onOffsetChanged: " + verticalOffset + " density:" + density);
+                int height = mCollapsing.getHeight();
+//                Log.d(TAG, "onOffsetChanged: " + height);
+
+
+                float offsetPercent = 1 - ((float) (Math.abs(verticalOffset) / density) / 60);
+
+                if (offsetPercent == mLastOffsetPercent) {
+                    return;
+                }
+
+                // title
+                float dimension = 26; // getResources().getDimension(R.dimen.text_title);
+                float minDimension = 18;
+                mToolbarTitle.setTextSize(minDimension + (dimension - minDimension) * offsetPercent);
+
+                RelativeLayout.LayoutParams lp = ((RelativeLayout.LayoutParams) mToolbarTitle.getLayoutParams());
+                int marginTop = ((int) ((30 - 20) * density * offsetPercent));
+                lp.setMargins(0, marginTop, 0, 0);
+                mToolbarTitle.setLayoutParams(lp);
+                //
+
+                // edit
+                if (offsetPercent <= 0.1) {
+                    mToolbarEdit.setVisibility(View.GONE);
+                } else {
+                    mToolbarEdit.setVisibility(View.VISIBLE);
+                }
+                mToolbarEdit.setAlpha(offsetPercent);
+
+                // toolbar
+                ViewGroup.LayoutParams toolbarLP = mToolbar.getLayoutParams();
+                int toolbarHeight = ((int) ((80 + (110 - 80) * offsetPercent) * density));
+                toolbarLP.height = toolbarHeight;
+                mToolbar.setLayoutParams(toolbarLP);
+
+                //ImageView
+                mIvToolbar.setScaleX(offsetPercent);
+                mIvToolbar.setScaleY(offsetPercent);
+                mIvToolbar.setAlpha(offsetPercent);
+
+
+                Log.d(TAG, "onOffsetChanged: offsetPercent" + offsetPercent +
+                        "title size:" + dimension + " percent:" + dimension * offsetPercent);
+                mLastOffsetPercent = offsetPercent;
+            }
+        });
+
+
+        initRV();
+    }
+
+    private void initRV() {
+        SimpleTextAdapter simpleTextAdapter = new SimpleTextAdapter();
+        mRv.setAdapter(simpleTextAdapter);
+    }
+
+    @OnClick(R.id.fab_youdao)
+    public void onViewClicked() {
+        Toast.makeText(this, "fab click", Toast.LENGTH_SHORT).show();
     }
 }
