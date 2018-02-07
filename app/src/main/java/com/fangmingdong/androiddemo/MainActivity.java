eeacc47f06df8047fccda7e20b9e6e1702017e04
方明东 <fangmingdong0224@gmail.com>
2018-02-07 11:18:09 +0800

    add

--- app/src/main/java/com/fangmingdong/androiddemo/MainActivity.java
+++ app/src/main/java/com/fangmingdong/androiddemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.fangmingdong.androiddemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.fangmingdong.androiddemo.constainslayoutAnimate.ConstraintLayoutAnimateActivity;
@@ -11,8 +14,14 @@ import com.fangmingdong.androiddemo.recyclerViewSideScroll.RecyclerViewSideScrol
 import com.fangmingdong.androiddemo.scrollView.ScrollViewActivity;
 import com.fangmingdong.androiddemo.textDraw.TextDrawActivity;
 import com.fangmingdong.androiddemo.weixinxiaochengxu.WeiXinActivity;
+import com.jakewharton.disklrucache.DiskLruCache;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +35,40 @@ public class MainActivity extends AppCompatActivity {
 //        mOverScroller.startScroll(0, 0,100, 100, 500);
 
 
+
+
+        try {
+            diskLruCache();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e(TAG, "onCreate: ", e);
+        }
+
     }
+
+    private void diskLruCache() throws Exception {
+        File directory = getCacheDir();
+        int appVersion = 1;
+        int valueCount = 1;
+        long maxSize = 10 * 1024;
+        DiskLruCache diskLruCache = DiskLruCache.open(directory, appVersion, valueCount, maxSize);
+
+        DiskLruCache.Editor editor = diskLruCache.edit(String.valueOf(System.currentTimeMillis()));
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(editor.newOutputStream(0));
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.img);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bufferedOutputStream);
+
+        editor.commit();
+        diskLruCache.flush();
+        diskLruCache.close();
+
+
+
+
+
+    }
+
+    
 
     public void toCoordinatorLayout(View view) {
         CoordinatorLayoutActivity.actionStart(this);
